e0ce4a1722d63901befe733977230e4927ae0dc6
Oliver Lorenz <lorenz@cybus.io>
2016-11-04 22:16:13 +0100

    added tests

---
+++ test/helper/AbstractTest.js
@@ -0,0 +1,85 @@
+/* global describe, it, assert */
+
+'use strict'
+const assert = require('assert')
+
+module.exports = (() => {
+  function weightTest (asset, list) {
+    const description = `check "${asset}" with "${list}"-list`
+    describe(description, () => {
+      const assets = require(`../assets/${asset}`)
+      const sentiment = require('../../../sentiment-custom-lists/lib')
+
+      Object.keys(assets).forEach((key) => {
+        const assetItem = assets[key]
+        const result = sentiment(assetItem.payload, list)
+        const pattern = /^([<>=!]+)([0-9-]+)$/g
+        const [matches, operator, score] = pattern.exec(assetItem.score)
+        describe(result.debugMessage + ` (${operator}${score})`.grey, () => {
+          switch (operator) {
+            case '<':
+              it(`score should be smaller than "${score}"`, () => {
+                try {
+                  assert.ok(result.score < score, result)
+                } catch (err) {
+                  throw err
+                }
+              })
+              break
+            case '<=':
+              it(`score should be smaller or the same as "${score}"`, () => {
+                try {
+                  assert.ok(result.score <= score, result)
+                } catch (err) {
+                  throw err
+                }
+              })
+              break
+            case '=':
+            case '==':
+            case '===':
+              it(`score should be equal "${score}"`, () => {
+                try {
+                  assert.equal(result.score, parseInt(score, 10), result)
+                } catch (err) {
+                  throw err
+                }
+              })
+              break
+            case '>':
+              it(`score should be greater than "${score}"`, () => {
+                try {
+                  assert.ok(result.score > score, result)
+                } catch (err) {
+                  throw err
+                }
+              })
+              break
+            case '>=':
+              it(`score should be greater or the same as "${score}"`, () => {
+                try {
+                  assert.ok(result.score >= score, result)
+                } catch (err) {
+                  throw err
+                }
+              })
+              break
+            case '!':
+              it(`score should be not the same as "${score}"`, () => {
+                try {
+                  assert.ok(result.score !== score, result)
+                } catch (err) {
+                  throw err
+                }
+              })
+              break
+          }
+        })
+      })
+    })
+  }
+
+  return {
+    weightTest
+  }
+})()
---
+++ test/weights/de_de-public-transport/asset/tweets/HVVStoerungen.js
@@ -0,0 +1,4 @@
+'use strict'
+
+const weightTest = require('../../../../helper/AbstractTest').weightTest
+weightTest('tweets/@HVVStoerungen.json', 'de_de-public-transport')
